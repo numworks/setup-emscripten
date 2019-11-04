b0fb65f26b23e1f940f9711097e21d8f917116b4
Romain Goyet <romain.goyet@numworks.com>
2019-11-04 15:34:38 +0100

    Allow specifying an SDK

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Setup emscripten
 
-This is a GitHub action to setup an emscripten environment
+This is a GitHub action to setup an emscripten environment.
+You can optionally specify which sdk you want to install.
 
 In actions.yml:
 
 ```
 steps:
   - uses: numworks/setup-emscripten@v1
-  - run: source $EMSDK/emsdk_env.sh && emcc --version
+    with:
+      sdk: latest-fastcomp
+  - run: emcc --version
 ```
--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-const core = require('@actions/core');
-const exec = require('@actions/exec');
-const tc = require('@actions/tool-cache');
-
-const tempDirectory = process.env['RUNNER_TEMP'] || '';
+const core = require('@actions/core')
+const exec = require('@actions/exec')
+const tc = require('@actions/tool-cache')
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
 
 async function run() {
   try {
+    const sdk = core.getInput('sdk')
     await exec.exec('git', ['clone', 'https://github.com/emscripten-core/emsdk.git'])
-    const cachedPath = await tc.cacheDir('emsdk', 'emscripten', 'latest');
-    core.addPath(cachedPath);
-    core.exportVariable('EMSDK', cachedPath);
-    await exec.exec('emsdk', ['install', 'latest'])
-    await exec.exec('emsdk', ['activate', 'latest'])
-  }
-  catch (error) {
+    const emSdk = await tc.cacheDir('emsdk', 'emscripten', 'latest');
+
+    core.exportVariable('EMSDK', emSdk)
+    core.addPath(emSdk)
+
+    await exec.exec('emsdk', ['install', sdk])
+    await exec.exec('emsdk', ['activate', sdk])
+
+    const emConfPath = path.join(os.homedir(), '.emscripten')
+    core.exportVariable('EM_CONFIG', emConfPath)
+    const emConf = fs.readFileSync(emConfPath).toString()
+    const emRoot = emConf.match(/EMSCRIPTEN_ROOT = '(.*)'/)[1]
+    core.addPath(emRoot)
+    const emNode = emConf.match(/NODE_JS = '(.*)'/)[1]
+    core.exportVariable('EMSDK_NODE', emNode)
+    core.addPath(path.dirname(emNode))
+  } catch (error) {
     core.setFailed(error.message);
   }
 }
