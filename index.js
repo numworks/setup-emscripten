3f96d047259bdede10d5ef117effedd06f2936a9
Émilie Feral <emilie.feral@numworks.com>
2020-06-02 10:23:06 +0200

    [index.js] .emscripten is no longer in home directory but in the emSdk installation folder

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ async function run() {
     await exec.exec('emsdk', ['install', sdk])
     await exec.exec('emsdk', ['activate', sdk])
 
-    const emConfPath = path.join(os.homedir(), '.emscripten')
+    const emConfPath = path.join(emSdk, '.emscripten')
     core.exportVariable('EM_CONFIG', emConfPath)
     const emConf = fs.readFileSync(emConfPath).toString()
-    const emRoot = emConf.match(/EMSCRIPTEN_ROOT = '(.*)'/)[1]
+    const emRoot = path.join(emSdk, emConf.match(/EMSCRIPTEN_ROOT = emsdk_path \+ '(.*)'/)[1])
     core.addPath(emRoot)
-    const emNode = emConf.match(/NODE_JS = '(.*)'/)[1]
+    const emNode = path.join(emSdk, emConf.match(/NODE_JS = emsdk_path \+ '(.*)'/)[1])
     core.exportVariable('EMSDK_NODE', emNode)
     core.addPath(path.dirname(emNode))
   } catch (error) {
